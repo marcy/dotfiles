fe1407147d08cb33817310e7a10c914089d24427
marcy <masashi.oyamada@gmail.com>
2017-02-01 09:29:27 +0900

    setopt

--- dot.zshrc
+++ dot.zshrc
@@ -1,9 +1,10 @@
 export ZPLUG_HOME=/usr/local/opt/zplug
 source $ZPLUG_HOME/init.zsh
 
-zplug 'marcy/dotfiles', use:local.zsh
-
+zplug 'chrissicool/zsh-256color', use:zsh-256color.plugin.zsh
 zplug 'dracula/zsh', as:theme
+
+zplug 'marcy/dotfiles', use:local.zsh
 
 # 未インストール項目をインストールする
 if ! zplug check --verbose; then
--- local.zsh
+++ local.zsh
@@ -1,3 +1,13 @@
+setopt hist_ignore_dups      # 重複を記録しない
+setopt hist_ignore_all_dups  # ヒストリに追加されるコマンド行が古いものと同じなら古いものを削除
+setopt hist_ignore_space     # スペースで始まるコマンド行はヒストリリストから削除
+setopt hist_verify           # ヒストリを呼び出してから実行する間に一旦編集可能
+setopt hist_reduce_blanks    # 余分な空白は詰めて記録
+setopt hist_save_no_dups     # 古いコマンドと同じものは無視
+setopt hist_no_store         # historyコマンドは履歴に登録しない
+setopt hist_expand           # 補完時にヒストリを自動的に展開
+setopt inc_append_history    # 履歴をインクリメンタルに追加
+
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' # 補完時に大文字小文字を区別しない
 
 export LANG=ja_JP.UTF-8
