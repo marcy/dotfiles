343a6c6d9a71b3c9659e6112e0e1aba4aa784e3e
marcy <masashi.oyamada@gmail.com>
2017-05-18 18:57:24 +0900

    modify linkmaker

--- linkmaker.sh
+++ linkmaker.sh
@@ -2,17 +2,11 @@
 
 echo 'link maker start'
 
-ln -s $(PWD)/../dot.emacs.d ~/.emacs.d
-
-ln -s $(PWD)/Brewfile ~/Brewfile
-ln -s $(PWD)/dot.gemrc ~/.gemrc
-ln -s $(PWD)/dot.gitconfig ~/.gitconfig.global
-ln -s $(PWD)/dot.gitignore_global ~/.gitignore_global
-ln -s $(PWD)/dot.home.gitconfig ~/.gitconfig
-ln -s $(PWD)/dot.irbrc ~/.irbrc
-ln -s $(PWD)/dot.my.cnf ~/.my.cnf
-ln -s $(PWD)/dot.rubocop.yml ~/.rubocop.yml
-ln -s $(PWD)/dot.screenrc ~/.screenrc
-ln -s $(PWD)/dot.tigrc ~/.tigrc
+ln -fs $(PWD)/dot.gemrc ~/.gemrc
+ln -fs $(PWD)/dot.gitignore_global ~/.gitignore_global
+ln -fs $(PWD)/dot.irbrc ~/.irbrc
+ln -fs $(PWD)/dot.my.cnf ~/.my.cnf
+ln -fs $(PWD)/dot.screenrc ~/.screenrc
+ln -fs $(PWD)/dot.tigrc ~/.tigrc
 
 echo "done"
