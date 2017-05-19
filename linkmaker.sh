5321b62621f173d8b601daaac7c6a354b9c54a12
marcy <masashi.oyamada@gmail.com>
2017-05-19 11:57:31 +0900

    modify linkmarker

--- linkmaker.sh
+++ linkmaker.sh
@@ -4,6 +4,7 @@ echo 'link maker start'
 
 ln -fns $(PWD)/dot.config ~/.config
 ln -fns $(PWD)/dot.emacs.d ~/.emacs.d
+
 ln -fs $(PWD)/dot.gemrc ~/.gemrc
 ln -fs $(PWD)/dot.gitignore_global ~/.gitignore_global
 ln -fs $(PWD)/dot.irbrc ~/.irbrc
