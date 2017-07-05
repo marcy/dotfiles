1b1fc6195059fe305ca51cd40b987ddad1720924
marcy <masashi.oyamada@gmail.com>
2017-07-05 18:52:37 +0900

    flycheck rubocop

--- dot.emacs.d/inits/33-python.el
+++ dot.emacs.d/inits/33-python.el
@@ -1,4 +1,9 @@
-(require 'py-autopep8)
+(require 'python)
 
+(require 'py-autopep8)
 ;; 保存時にバッファ全体を自動整形する
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+
+(defun tnoda/turn-on-flycheck-mode ()
+  (flycheck-mode 1))
+(add-hook 'python-mode-hook 'tnoda/turn-on-flycheck-mode)
--- dot.emacs.d/inits/33-ruby.el
+++ dot.emacs.d/inits/33-ruby.el
@@ -61,3 +61,27 @@
 
 (require 'rubocop)
 (add-hook 'ruby-mode-hook 'rubocop-mode)
+
+;; flycheck
+(require 'flycheck)
+
+(add-hook 'ruby-mode-hook 'flycheck-mode)
+(flycheck-define-checker ruby-rubocop
+  "A Ruby syntax and style checker using the RuboCop tool.
+
+See URL `http://batsov.com/rubocop/'."
+  :command ("rubocop" "--display-cop-names" "--format" "emacs"
+            (config-file "--config" flycheck-rubocoprc)
+            (option-flag "--lint" flycheck-rubocop-lint-only)
+            "--stdin" source-original)
+  :standard-input t
+  :error-patterns
+  ((info line-start (file-name) ":" line ":" column ": C: "
+         (optional (id (one-or-more (not (any ":")))) ": ") (message) line-end)
+   (warning line-start (file-name) ":" line ":" column ": W: "
+            (optional (id (one-or-more (not (any ":")))) ": ") (message)
+            line-end)
+   (error line-start (file-name) ":" line ":" column ": " (or "E" "F") ": "
+          (optional (id (one-or-more (not (any ":")))) ": ") (message)
+          line-end))
+  :modes (enh-ruby-mode ruby-mode))
