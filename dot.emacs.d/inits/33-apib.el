e59c9fb2f5eba0f11ba39ee8d88b0fc1d9e286f7
marcy <masashi.oyamada@gmail.com>
2018-04-05 17:12:14 +0900

    [emacs]apib mode

---
+++ dot.emacs.d/inits/33-apib.el
@@ -0,0 +1,4 @@
+(autoload 'apib-mode "apib-mode"
+        "Major mode for editing API Blueprint files" t)
+(add-to-list 'auto-mode-alist '("\\.apib\\'" . apib-mode))
+(custom-set-variables '(apib-drafter-executable "aglio"))
