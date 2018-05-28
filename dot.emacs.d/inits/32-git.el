3a7e13e154b21bf5447b011aa2a5ccf96ffd92dc
marcy <masashi.oyamada@gmail.com>
2018-05-28 12:18:38 +0900

    vc-annotateで現在の行がmergeされたPRを開く

--- dot.emacs.d/inits/32-git.el
+++ dot.emacs.d/inits/32-git.el
@@ -10,3 +10,14 @@
 ;;      (set-face-foreground 'magit-diff-add "#40ff40")
 ;;      (set-face-foreground 'magit-diff-del "#ff4040")
 ;;      (set-face-foreground 'magit-diff-file-header "#4040ff")))
+
+;; vc-annotateで現在の行がmergeされたPRを開く
+(require 'vc-annotate)
+
+(defun vc-annotate-open-pr-at-line ()
+  (interactive)
+  (let* ((rev-at-line (vc-annotate-extract-revision-at-line))
+         (rev (car rev-at-line)))
+    (shell-command (concat "open-pr-from-commit " rev))))
+
+(define-key vc-annotate-mode-map (kbd "P") 'vc-annotate-open-pr-at-line)
