fb19d0fd9db1c08c3e461891592b43095cbb2eb3
marcy <masashi.oyamada@gmail.com>
2018-06-12 12:35:41 +0900

    [emacs]change theme

--- dot.emacs.d/custom.el
+++ dot.emacs.d/custom.el
@@ -9,13 +9,14 @@
  '(anzu-deactivate-region t)
  '(anzu-mode-lighter "")
  '(anzu-search-threshold 1000)
+ '(apib-drafter-executable "aglio")
  '(custom-safe-themes
    (quote
     ("5d3e0746023fc5e246eb3e0e48c1ccb5ce0387fc4273896c6cf02ee349c2eba8" default)))
  '(init-loader-show-log-after-init (quote error-only))
  '(package-selected-packages
    (quote
-    (apib-mode migemo lua-mode package-build shut-up epl git commander f s helm-rb keyfreq color-theme-modern elpy py-autopep8 highlight-symbol mark-multiple expand-region bind-key company-go dash yaml-mode wgrep-ag web-mode smart-newline sequential-command scss-mode scratch-ext ruby-end ruby-block rubocop rspec-mode rinari pallet osx-dictionary open-junk-file multicolumn markdown-mode magit js2-mode init-loader howm helm-projectile helm-ls-git helm-ghq helm-bundle-show helm-ag-r helm-ag haskell-mode haml-mode go-autocomplete gitignore-mode github-browse-file gitconfig-mode git-gutter git-gutter+ gh flycheck fish-mode exec-path-from-shell elscreen elixir-mode dumb-jump dired+ color-moccur coffee-mode auto-highlight-symbol anzu ag 2048-game)))
+    (dracula-theme apib-mode migemo lua-mode package-build shut-up epl git commander f s helm-rb keyfreq color-theme-modern elpy py-autopep8 highlight-symbol mark-multiple expand-region bind-key company-go dash yaml-mode wgrep-ag web-mode smart-newline sequential-command scss-mode scratch-ext ruby-end ruby-block rubocop rspec-mode rinari pallet osx-dictionary open-junk-file multicolumn markdown-mode magit js2-mode init-loader howm helm-projectile helm-ls-git helm-ghq helm-bundle-show helm-ag-r helm-ag haskell-mode haml-mode go-autocomplete gitignore-mode github-browse-file gitconfig-mode git-gutter git-gutter+ gh flycheck fish-mode exec-path-from-shell elscreen elixir-mode dumb-jump dired+ color-moccur coffee-mode auto-highlight-symbol anzu ag 2048-game)))
  '(ruby-insert-encoding-magic-comment nil)
  '(send-mail-function (quote smtpmail-send-it)))
 (custom-set-faces
@@ -23,6 +24,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(helm-selection ((t (:background "dark blue" :underline nil))))
  '(markdown-header-delimiter-face ((t (:inherit org-mode-line-clock))))
  '(markdown-header-face-1 ((t (:inherit outline-1 :weight bold))))
  '(markdown-header-face-2 ((t (:inherit outline-2 :weight bold))))
--- dot.emacs.d/inits/23-frame.el
+++ dot.emacs.d/inits/23-frame.el
@@ -55,18 +55,21 @@
 ;;; ============================================================
 (when (eq window-system 'ns)
   ;; 透明に
+  ;(set-frame-parameter nil 'alpha '(100 100)))
   (set-frame-parameter nil 'alpha '(90 75)))
 
 ;; color-theme-modern
-(load-theme 'dark-laptop t t)
-(enable-theme 'dark-laptop)
+;(load-theme 'dark-laptop t t)
+;(enable-theme 'dark-laptop)
+(load-theme 'dracula t t)
+(enable-theme 'dracula)
 
 ;; EmacsにFocusが外れている際のFace
 (defun my-out-focused-mode-line()
   (set-face-background 'mode-line "purple3"))
 ;; EmacsにFocusが当たっている際のFace
 (defun my-in-focused-mode-line()
-  (set-face-background 'mode-line "white"))
+  (set-face-background 'mode-line "black"))
 
 (add-hook 'focus-out-hook 'my-out-focused-mode-line)
 (add-hook 'focus-in-hook 'my-in-focused-mode-line)
