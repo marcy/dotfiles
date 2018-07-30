31137b9b2b9a4a9ab651abfafecf901babe00c68
marcy <masashi.oyamada@gmail.com>
2018-07-30 14:27:16 +0900

    [emacs]markdownl mode

--- dot.emacs.d/custom.el
+++ dot.emacs.d/custom.el
@@ -19,17 +19,3 @@
     (dracula-theme apib-mode migemo lua-mode package-build shut-up epl git commander f s helm-rb keyfreq color-theme-modern elpy py-autopep8 highlight-symbol mark-multiple expand-region bind-key company-go dash yaml-mode wgrep-ag web-mode smart-newline sequential-command scss-mode scratch-ext ruby-end ruby-block rubocop rspec-mode rinari pallet osx-dictionary open-junk-file multicolumn markdown-mode magit js2-mode init-loader helm-projectile helm-ls-git helm-ghq helm-bundle-show helm-ag-r helm-ag haskell-mode haml-mode go-autocomplete gitignore-mode github-browse-file gitconfig-mode git-gutter git-gutter+ gh flycheck fish-mode exec-path-from-shell elscreen elixir-mode dumb-jump dired+ color-moccur coffee-mode auto-highlight-symbol anzu ag 2048-game)))
  '(ruby-insert-encoding-magic-comment nil)
  '(send-mail-function (quote smtpmail-send-it)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-selection ((t (:background "dark blue" :underline nil))))
- '(markdown-header-delimiter-face ((t (:inherit org-mode-line-clock))))
- '(markdown-header-face-1 ((t (:inherit outline-1 :weight bold))))
- '(markdown-header-face-2 ((t (:inherit outline-2 :weight bold))))
- '(markdown-header-face-3 ((t (:inherit outline-3 :weight bold))))
- '(markdown-header-face-4 ((t (:inherit outline-4 :weight bold))))
- '(markdown-header-face-5 ((t (:inherit outline-5 :weight bold))))
- '(markdown-header-face-6 ((t (:inherit outline-6 :weight bold))))
- '(markdown-pre-face ((t (:inherit org-formula)))))
--- dot.emacs.d/inits/33-markdown.el
+++ dot.emacs.d/inits/33-markdown.el
@@ -1,20 +1,13 @@
+(setq markdown-asymmetric-header t)
+;(setq markdown-marginalize-headers t)
+(setq markdown-header-scaling t)
+
 (setq auto-mode-alist (append
                        auto-mode-alist
                        (list
                         '("\\.md" . markdown-mode)
                         '("\\.markdown" . markdown-mode)
                         )))
-
-(custom-set-faces
- '(markdown-header-delimiter-face ((t (:inherit org-mode-line-clock))))
- '(markdown-header-face-1 ((t (:inherit outline-1 :weight bold))))
- '(markdown-header-face-2 ((t (:inherit outline-2 :weight bold))))
- '(markdown-header-face-3 ((t (:inherit outline-3 :weight bold))))
- '(markdown-header-face-4 ((t (:inherit outline-4 :weight bold))))
- '(markdown-header-face-5 ((t (:inherit outline-5 :weight bold))))
- '(markdown-header-face-6 ((t (:inherit outline-6 :weight bold))))
- '(markdown-pre-face ((t (:inherit org-formula))))
- )
 
 (if (executable-find "pandoc")
     (setq markdown-command "pandoc -s --self-contained -t html5 -c ~/.pandoc/github-markdown.css"))
