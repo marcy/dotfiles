9e73d9d05e0f63180331b90b44e766835f33f917
marcy <masashi.oyamada@gmail.com>
2019-02-06 17:50:06 +0900

    [emacs]remove unnecessary code

--- dot.emacs.d/inits/32-git.el
+++ dot.emacs.d/inits/32-git.el
@@ -4,13 +4,6 @@
 
 ;;; magit
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
-;(eval-after-load 'magit
-;  '(progn
-;     (set-face-background 'magit-item-highlight "#000000") ; 選択項目ハイライトがうっとうしいので背景色と同化
-     ;(set-face-background 'magit-item-highlight "#202020")
-;     (set-face-foreground 'magit-diff-add "#40ff40")
-;     (set-face-foreground 'magit-diff-del "#ff4040")
-;     (set-face-foreground 'magit-diff-file-header "#4040ff")))
 
 ;; vc-annotateで現在の行がmergeされたPRを開く
 (require 'vc-annotate)
