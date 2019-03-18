d889da66e68380597fa335edd94a31040224f446
marcy <masashi.oyamada@gmail.com>
2019-03-18 18:25:48 +0900

    [emacs]add witch-key.el
    * wihch-key.el を追加
    * 23-frame.el を init.el へ移動した

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -1,8 +1,3 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
 (require 'cask)
@@ -16,15 +11,90 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-;; init loader
+;;;;; init loader
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
 
-;; after init
+;;;;; after init
 (add-hook 'after-init-hook
   (lambda ()
     ;; show init time
     (message "init time: %.3f sec"
              (float-time (time-subtract after-init-time before-init-time)))))
+
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+
+(column-number-mode 1)          ; カーソルの位置が何文字目かを表示する
+(line-number-mode 1)            ; カーソルの位置が何行目かを表示する
+
+(unless (eq window-system nil)
+  (progn
+    (menu-bar-mode nil)        ; メニューバーなし
+    (tool-bar-mode -1)         ; ツールバーなし
+    (set-scroll-bar-mode nil)  ; スクロールバーなし
+    ))
+
+;; バッファの終わりをフリンジに表示
+(setq-default indicate-buffer-boundaries
+              '((top . nil) (bottom . right) (down . right)))
+
+;; color
+(show-paren-mode t)                     ; 括弧に色
+(setq show-paren-style 'parenthesis)
+(global-font-lock-mode t)               ; 字に色
+
+;;; リージョンの色
+(transient-mark-mode t)
+
+;;; 右端で折り返さない
+(setq truncate-lines nil)
+
+;; 分割したウィンドウでも右端で折り返さない
+(setq truncate-partial-width-windows nil)
+
+;; タブではなくスペースを使う
+(setq-default indent-tabs-mode nil)
+
+;; Cocoa Emacsのフォントセットを定義
+(when (eq window-system 'ns)
+  (create-fontset-from-ascii-font
+   "Menlo-12:weight=normal:slant=normal"
+   nil
+   "menlokakugo")
+  (set-fontset-font
+   "fontset-menlokakugo"
+   'unicode
+   (font-spec :family "Hiragino Kaku Gothic ProN" :size 12)
+   nil
+   'append)
+  (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
+  (add-to-list 'initial-frame-alist '(font . "fontset-menlokakugo"))
+  (add-to-list 'face-font-rescale-alist
+               '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
+  (add-hook 'after-init-hook
+            (lambda () (set-frame-font "fontset-menlokakugo"))))
+
+;; frame
+(when (eq window-system 'ns)
+  (set-frame-parameter nil 'alpha '(95 75))) ; 透明に
+
+(load-theme 'dracula t t)
+(enable-theme 'dracula)
+
+;; EmacsにFocusが外れている際のFace
+(defun my-out-focused-mode-line()
+  (set-face-background 'mode-line "purple4"))
+;; EmacsにFocusが当たっている際のFace
+(defun my-in-focused-mode-line()
+  (set-face-background 'mode-line "purple1"))
+
+(add-hook 'focus-out-hook 'my-out-focused-mode-line)
+(add-hook 'focus-in-hook 'my-in-focused-mode-line)
+
+;;;;; which-key-mode
+;(which-key-setup-side-window-bottom) ;ミニバッファ
+(which-key-setup-side-window-right) ;右側
+;(which-key-setup-side-window-right-bottom) ;両方使う
+
+(which-key-mode 1)
