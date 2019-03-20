08ea603b85cca5b3e3675aa799148d7fcc504de7
marcy <masashi.oyamada@gmail.com>
2019-03-20 12:40:30 +0900

    [emacs]add dumb-jump

---
+++ dot.emacs.d/inits/46-dumb-jump.el
@@ -0,0 +1,7 @@
+(require 'dumb-jump)
+;; これをしないとホームディレクトリ以下が検索対象になる
+(setq dumb-jump-default-project "")
+;; 日本語を含むパスだとgit grepがちゃんと動かない…
+(setq dumb-jump-force-searcher 'ag)
+;; 標準キーバインドを有効にする
+(dumb-jump-mode)
