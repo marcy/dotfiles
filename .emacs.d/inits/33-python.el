39d468b02257d05e3e56bf05b4ab3301c77558dc
marcy <masashi.oyamada@gmail.com>
2019-04-15 11:33:52 +0900

    [emacs]remove python settings

--- .emacs.d/inits/33-python.el
+++
@@ -1,6 +0,0 @@
-;; 保存時にバッファ全体を自動整形する
-(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
-
-(defun tnoda/turn-on-flycheck-mode ()
-  (flycheck-mode 1))
-(add-hook 'python-mode-hook 'tnoda/turn-on-flycheck-mode)
