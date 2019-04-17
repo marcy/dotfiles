0f5ac32392bd4bd13a9122caf55ffdcec9abce8e
marcy <masashi.oyamada@gmail.com>
2019-04-17 13:02:53 +0900

    [emacs]remove window-resizer

--- .emacs.d/inits/99-global-keys.el
+++ .emacs.d/inits/99-global-keys.el
@@ -11,7 +11,6 @@
 (bind-key "C-q" 'neotree-toggle)
 (bind-key "C-M-i" 'company-complete)
 ;(bind-key "" 'highlight-symbol-at-point)
-(bind-key "C-c C-r" 'window-resizer)
 (bind-key "C-x g" 'ag)
 (bind-key "C-;" 'helm-mini)
 (bind-key "C-:" 'helm-projectile)
