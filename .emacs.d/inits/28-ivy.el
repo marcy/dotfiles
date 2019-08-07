4d467bb5ded9288e8e10f0e1ff16a38318c50d2d
marcy <masashi.oyamada@gmail.com>
2019-08-07 18:16:10 +0900

    [emacs]ivy

--- .emacs.d/inits/28-ivy.el
+++ .emacs.d/inits/28-ivy.el
@@ -1,6 +1,9 @@
-(setq ivy-height 30)
-
-(setq dumb-jump-selector 'ivy)
+(setq ivy-fixed-height-minibuffer t
+      ivy-height 20
+      dumb-jump-selector 'ivy
+      smex-history-length 35
+      smex-completion-method 'ivy
+      counsel-yank-pop-separator "\n-------\n")
 
 (when (require 'all-the-icons-ivy nil t)
   (dolist (command '(counsel-projectile-switch-project
