a6580528e7e7f6f637acb927ae6d3065d76e6469
marcy <masashi.oyamada@gmail.com>
2019-08-13 14:37:32 +0900

    Revert "[emacs]ivy"
    This reverts commit e51f1974e72b488cb7778250d51649d758868ed1.

--- .emacs.d/inits/28-ivy.el
+++
@@ -1,9 +0,0 @@
-(setq ivy-height 30)
-
-(setq dumb-jump-selector 'ivy)
-
-(when (require 'all-the-icons-ivy nil t)
-  (dolist (command '(counsel-projectile-switch-project
-                     counsel-ibuffer))
-    (add-to-list 'all-the-icons-ivy-buffer-commands command))
-  (all-the-icons-ivy-setup))
