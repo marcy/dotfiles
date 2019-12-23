83ddf4b86ea88bcad98f4d20383dbbcaa5369ed3
marcy <masashi.oyamada@gmail.com>
2019-12-23 18:08:34 +0900

    [emacs] add rjsx-mode

--- .emacs.d/inits/33-javascript.el
+++ .emacs.d/inits/33-javascript.el
@@ -1,4 +1,6 @@
 (setq js-indent-level 2)
 
+(add-to-list 'auto-mode-alist '("\\.js$" . rjsx-mode))
+
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
